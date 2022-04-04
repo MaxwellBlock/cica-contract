172b02ab7e2cc3036510730bce77fddbb7c517f1
notnominated <notnominated@protonmail.com>
2022-04-04 21:05:33 +0800

    fix

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,8 +2,4 @@ const Hello = artifacts.require("Hello");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Hello);
-
-  await Hello.deployed().then(async function (instance) {
-    await instance.setCounter();
-  });
 };
