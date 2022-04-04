7785209e739731aafa2b164ab5fb51562ba89877
notnominated <notnominated@protonmail.com>
2022-04-04 17:33:47 +0800

    fix

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const https = require('https')
-
 const Hello = artifacts.require("Hello");
 
 module.exports = function (deployer) {
--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 const fs = require('fs');
-secrets = JSON.parse(fs.readFileSync('data.json','utf8'));
+secrets = JSON.parse(fs.readFileSync('secret.json','utf8'));
 
 module.exports = {
   /**
