b5a53143a20a56e6960b42f364d721e4f41982f2
notnominated <notnominated@protonmail.com>
2022-04-04 17:29:48 +0800

    fix

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,12 +5,4 @@ const Hello = artifacts.require("Hello");
 
 module.exports = function (deployer) {
   deployer.deploy(Hello);
-
-  let secrets = JSON.parse(fs.readFileSync('data.json','utf8'));
-  config = {
-    provider: secrets.bsctest_rpc,
-    hello: Hello.address
-  }
-
-  fs.writeFileSync('config.json', JSON.stringify(config), 'utf8')
 };
